284cacd7d5ad6d177aceb5068da9a0c81d9ba1d1
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-03 11:00:50 +0530

    Solved: Construct Uniform Parity Array I (leetcode)

---
+++ leetcode/Easy/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,14 @@
+// Problem: Construct Uniform Parity Array I
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: cpp
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/construct-uniform-parity-array-i/
+// Solved on: 2026-09-03T05:30:49.370Z
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+     return true;
+    }
+};
