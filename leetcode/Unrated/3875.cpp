02cb41cacbaf850cdd7faff8bfb61751fb6fc9cd
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-03 11:03:08 +0530

    Solved: Construct Uniform Parity Array l (leetcode)

---
+++ leetcode/Unrated/3875.cpp
@@ -0,0 +1,13 @@
+// Problem: Construct Uniform Parity Array l
+// Platform: leetcode
+// Language: C++17
+// Verdict: Accepted
+// URL: 
+// Solved on: 2026-09-03T05:33:07.334Z
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+     return true;
+    }
+};
