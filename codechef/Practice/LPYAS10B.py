442672d403547bf61888a512ad17911f2240dfde
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-18 22:08:03 +0530

    Solved: CodeChef Learn Problem Solving (codechef)

---
+++ codechef/Practice/LPYAS10B.py
@@ -0,0 +1,13 @@
+// Problem: CodeChef Learn Problem Solving
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LPYAS3/problems/LPYAS10B
+// Solved on: 2026-09-18T16:38:00.569Z
+
+# cook your dish here
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
