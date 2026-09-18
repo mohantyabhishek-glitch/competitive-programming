4b4ac01a60ba9ec391141311feed21ea24f03b0a
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-18 21:54:29 +0530

    Solved: CodeChef Learn Problem Solving (codechef)

---
+++ codechef/Practice/LPYAS59.py
@@ -0,0 +1,15 @@
+// Problem: CodeChef Learn Problem Solving
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LPYAS3/problems/LPYAS59
+// Solved on: 2026-09-18T16:24:26.973Z
+
+# cook your dish here
+a = int(input())
+if a > 0:
+    print("Positive")
+elif a == 0:
+    print("Zero")
+else:
+    print("Negative")
