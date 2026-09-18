7e458075b6b45b25bc4b8a6a300060854aaac2f6
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-18 22:11:36 +0530

    Solved: CodeChef Learn Problem Solving (codechef)

---
+++ codechef/Practice/LPYAS40B.py
@@ -0,0 +1,10 @@
+// Problem: CodeChef Learn Problem Solving
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LPYAS3/problems/LPYAS40B
+// Solved on: 2026-09-18T16:41:34.170Z
+
+# cook your dish here
+a = "I Love Python"
+print(a[::-1])
