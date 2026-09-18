d90c19eaff60330b48984fe5bd19ac2082e5d3aa
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-18 22:09:24 +0530

    Solved: CodeChef Learn Problem Solving (codechef)

---
+++ codechef/Practice/LPYAS20B.py
@@ -0,0 +1,10 @@
+// Problem: CodeChef Learn Problem Solving
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LPYAS3/problems/LPYAS20B
+// Solved on: 2026-09-18T16:39:22.168Z
+
+a = 4
+for i in range(a):
+    print("*"*a)
