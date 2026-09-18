88708287f2f69ad8b418c8466f25d364c797f392
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-18 22:05:13 +0530

    Solved: CodeChef Learn Problem Solving (codechef)

---
+++ codechef/Practice/LPYAS109.py
@@ -0,0 +1,13 @@
+// Problem: CodeChef Learn Problem Solving
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LPYAS3/problems/LPYAS109?tab=ide
+// Solved on: 2026-09-18T16:35:11.370Z
+
+num = int(input())
+b = 1 
+
+while b <= num:
+    print(b*b, end = " ")
+    b+=1
