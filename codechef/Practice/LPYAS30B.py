6db08ad5c270dcfbcc856ac1d4938f18837c95ac
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-18 22:10:36 +0530

    Solved: CodeChef Learn Problem Solving (codechef)

---
+++ codechef/Practice/LPYAS30B.py
@@ -0,0 +1,11 @@
+// Problem: CodeChef Learn Problem Solving
+// Platform: codechef
+// Language: 20.25
+18.0
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LPYAS3/problems/LPYAS30B
+// Solved on: 2026-09-18T16:40:34.167Z
+
+a = 4.5
+print(a * a)
+print(a * 4)
