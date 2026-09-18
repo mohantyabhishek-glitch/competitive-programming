9f67a513a020dbeb56c1dac6012439b2da736448
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-18 21:47:35 +0530

    Solved: CodeChef Learn Problem Solving (codechef)

---
+++ codechef/Practice/CCLEARN.py
@@ -0,0 +1,9 @@
+// Problem: CodeChef Learn Problem Solving
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LPYAS3/problems/CCLEARN
+// Solved on: 2026-09-18T16:17:32.984Z
+
+a = int(input())
+print(a * 2)
