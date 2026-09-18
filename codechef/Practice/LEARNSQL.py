8c72c4dd2aedb75768c7cc6fdbdb26387478d66d
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-18 21:48:53 +0530

    Solved: CodeChef Learn Problem Solving (codechef)

---
+++ codechef/Practice/LEARNSQL.py
@@ -0,0 +1,9 @@
+// Problem: CodeChef Learn Problem Solving
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LPYAS3/problems/LEARNSQL
+// Solved on: 2026-09-18T16:18:50.975Z
+
+a, b, c = list(map(int, input().split()))
+print((a+c)*b)
