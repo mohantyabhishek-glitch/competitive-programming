e1626fe4251e7901e02a326b781fa0d76e3d726c
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-18 22:13:54 +0530

    Solved: CodeChef Learn Problem Solving (codechef)

---
+++ codechef/Practice/LPYAS100B.py
@@ -0,0 +1,13 @@
+// Problem: CodeChef Learn Problem Solving
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LPYAS3/problems/LPYAS100B
+// Solved on: 2026-09-18T16:43:52.178Z
+
+a = list(map(str, input().split()))
+b = input()
+if b not in a:
+    print("NO")
+else:
+    print("YES")
