6fbfdfb5e95d26b19c22d392098aa630258b86ab
mohantyabhishek-glitch <mohantysaroj236@gmail.com>
2026-09-19 22:03:39 +0530

    Solved: CHEFSCORE (codechef)

--- codechef/Practice/CHEFSCORE.cpp
+++ codechef/Practice/CHEFSCORE.cpp
@@ -3,21 +3,22 @@
 // Language: C++​
 // Verdict: Accepted
 // URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFSCORE
-// Solved on: 2026-09-18T16:09:04.146Z
+// Solved on: 2026-09-19T16:33:38.187Z
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
     int a;
     cin >> a;
     int b, c, d;
-    while(a--){
+    while (a--) {
         cin >> b >> c >> d;
-        if(d % c == 0){
+        if(d % c == 0 && d <= b * c){
             cout << "YES" << endl;
+        } else{
+            cout << "NO" << endl;
         }
-        else{
-        cout << "NO" << endl;}
     }
 }
